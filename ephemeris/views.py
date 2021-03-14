1df9f3a280f7288f3f2ee286828e6e617ee91cdd
PatrickBoynton <jpboynton3@gmail.com>
2021-03-14 14:35:11 -0400

    Added a list view and detail view.

--- ephemeris/views.py
+++ ephemeris/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from rest_framework import generics, permissions
+from .models import Ephem
+from .serializers import EphemSerializer
+
 
 # Create your views here.
+class EphemListView(generics.ListAPIView):
+    queryset = Ephem.objects.all()
+    serializer_class = EphemSerializer
+
+
+class EphemDetailView(generics.RetrieveAPIView):
+    queryset = Ephem.objects.all()
+    serializer_class = EphemSerializer
