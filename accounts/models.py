6491c9942ee754971636875fd1f23b0b0d16569a
PatrickBoynton <jpboynton3@gmail.com>
2021-03-13 23:14:57 -0500

    Added fields for equipment

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
 
 # Create your models here.
@@ -12,10 +13,11 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='profiles', null=True)
+    about_me = models.TextField(null=True, blank=True)
+    my_equipment = ArrayField(null=True, blank=True)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
 
     def __str__(self):
         return self.user.username
