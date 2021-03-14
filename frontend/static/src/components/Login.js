bc60146eedf135e7bc5fc5997a1341a0ca9b9268
PatrickBoynton <jpboynton3@gmail.com>
2021-03-14 17:46:55 -0400

    Added classnames to the forms.

--- frontend/static/src/components/Login.js
+++ frontend/static/src/components/Login.js
@@ -1,11 +1,19 @@
 import {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Login extends Component{
     render() {
         return (
-            <div>
-                Login
-            </div>
+            <>
+                <form className="text-center login-register" action="">
+                    <label className="form-label" htmlFor="username">Username</label>
+                    <input className="form-control" type="text" name="username"/>
+                    <label className="form-label" htmlFor="password">Password</label>
+                    <input className="form-control mb-3" type="password" name="" id=""/>
+                    <button className="btn btn-success disabled">Login</button>
+                    <p>Don't have an account yet? Why not <Link to="/register">Register</Link> First?</p>
+                </form>
+            </>
         );
     }
 }
--- frontend/static/src/components/Register.js
+++ frontend/static/src/components/Register.js
@@ -1,11 +1,23 @@
 import {Component} from 'react'
+import {Link} from 'react-router-dom';
 
 class Register extends Component{
     render() {
         return (
-            <div>
-                Register
-            </div>
+            <>
+                <form className="login-register" action="">
+                    <label className="form-label" htmlFor="username">Username</label>
+                    <input className="form-control" type="text" name="username"/>
+                    <label className="form-label" htmlFor="email">Email</label>
+                    <input className="form-control" type="email" name="email"/>
+                    <label className="form-label" htmlFor="password1">Password</label>
+                    <input className="form-control" type="password" name="password1"/>
+                    <label className="form-label" htmlFor="password2">Confirm  Password</label>
+                    <input className="form-control mb-3" ype="password" name="password2"/>
+                    <button className="btn btn-success disabled" type="submit">Register</button>
+                    <p>Already have an account? Why don't you <Link to="/login">Login</Link>? </p>
+                </form>
+            </>
         );
     }
 }
