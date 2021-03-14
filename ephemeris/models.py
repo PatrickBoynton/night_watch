65e8191d9d35c280ad291ce27bfbc6d542e57fbd
PatrickBoynton <jpboynton3@gmail.com>
2021-03-14 14:33:14 -0400

    Added a basic ephemeris class.

--- ephemeris/models.py
+++ ephemeris/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Ephem(models.Model):
+    name = models.CharField(max_length=40)
+    alt = models.FloatField()
+    az = models.FloatField()
+    is_visible = models.BooleanField()
+    magnitude = models.FloatField()
+    image = models.ImageField()
+    description = models.TextField()
