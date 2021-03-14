eb4f58e691ae6125cf0b270929b31db676981eeb
PatrickBoynton <jpboynton3@gmail.com>
2021-03-14 17:24:48 -0400

    Added react router.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
-
+import Register from './components/Register';
+import Login from './components/Login';
+import {Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path='/register' component={Register}/>
+        <Route path='/login' component={Login}/>
+      </Switch>
     </div>
   );
 }
