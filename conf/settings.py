ccb8130dd507b60c81d19d9e00e090d2387bc6c5
PatrickBoynton <jpboynton3@gmail.com>
2021-03-13 23:20:40 -0500

    Added the database url.

--- conf/settings.py
+++ conf/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
+DATABASE_URL = os.environ.get("DATABASE_URL")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
