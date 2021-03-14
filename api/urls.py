0a61b02d45010a816af06655d134e3050ee23833
PatrickBoynton <jpboynton3@gmail.com>
2021-03-14 17:03:29 -0400

    Fixed a spelling mistake.

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ app_name = "api_v1"
 urlpatterns = [
     # Short for ephemeris which
     # describes the various things about a celestial body.
-    path("ephim/", include("ephim.urls", namespace="ephim"))
+    path("ephem/", include("ephemeris.urls", namespace="ephemeris"))
 ]
