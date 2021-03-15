c5267baa15670c094e81f7934519403bb966bcac
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 09:55:58 -0400

    Added a basic event model.

--- events/models.py
+++ events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=60)
+    ephemeris = models.CharField(max_length=30)
+    time = models.TimeField()
+    description = models.TextField()
