426a498f1564974cda39396ec9d5710a7a79ddf5
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 17:02:55 -0400

    Set up a way to display the celestial bodies.

--- frontend/static/src/components/EphemList.js
+++ frontend/static/src/components/EphemList.js
@@ -1,13 +1,34 @@
 import {Component} from 'react';
 
-class EphemList extends Component{
+class EphemList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ephems: [],
+        };
+    }
+
+    async componentDidMount() {
+        const response = await fetch('api/v1/ephem/');
+        const data = await response.json();
+        this.setState({ephems: data});
+    }
+
     render() {
+        const list = this.state.ephems.map(item => <section key={item.id} className="row">
+            <div className="col">
+                <img src={item.image}/>
+                <h2>{item.name}</h2>
+                <p>rise time: {item.rise_time}</p>
+                <p>set time: {item.set_time}</p>
+            </div>
+        </section>);
         return (
-            <div>
-                EphemList
+            <div className="container">
+                {list}
             </div>
         );
     }
 }
 
-export default EphemList
+export default EphemList;
