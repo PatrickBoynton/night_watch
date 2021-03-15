19d61633c862955ef43aabd257b00ab4550ab0ad
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 07:51:58 -0400

    Still trying to fix the url problem.

--- conf/urls.py
+++ conf/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api/v1/', include('api.urls', namespace='api_v1')),
-                  path('', include('frontend.urls', namespace='frontend.urls')),
+
                   path('rest-auth/', include('rest_auth.urls')),
                   path('rest-auth/registration/',
                        include('rest_auth.registration.urls')),
+                  path('', include('frontend.urls', namespace='frontend')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
