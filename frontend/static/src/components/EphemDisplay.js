08abf397f41e63f0c99cca63d9b31cb8704057e9
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 15:41:40 -0400

    Set up a way for displaying an individual celestial body.

--- frontend/static/src/components/EphemDisplay.js
+++ frontend/static/src/components/EphemDisplay.js
@@ -1,11 +1,36 @@
 import {Component} from 'react';
 
 class EphemDisplay extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ephem: [],
+        }
+    }
+
+    async componentDidMount() {
+        const response =  await fetch("/api/v1/ephem/");
+        const data = await response.json();
+
+        this.setState({ephem: data})
+        console.log(this.state);
+    }
+
     render() {
+        const ephems = this.state.ephem.map(ephem => <section>
+            <img src={ephem.image} alt="The sun."/>
+            <h1>{ephem.name}</h1>
+            <p>Rise time: </p>
+            <h2>{ephem.rise_time}</h2>
+            <p>Set time: </p>
+            <h2>{ephem.set_time}</h2>
+        </section>);
         return (
-            <div>
-                EphemDisplay
-            </div>
+            <>
+                {ephems}
+            </>
         );
     }
 }
