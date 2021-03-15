2e66746b1666661690b53bcf5490c7ca76b9e008
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 15:41:53 -0400

    Created a list view for celestial bodies.

---
+++ frontend/static/src/components/EphemList.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class EphemList extends Component{
+    render() {
+        return (
+            <div>
+                EphemList
+            </div>
+        );
+    }
+}
+
+export default EphemList
