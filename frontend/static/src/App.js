d812068ea779ea6dea52c2d9c8d4f6c88a5cae0c
PatrickBoynton <jpboynton3@gmail.com>
2021-03-14 22:26:34 -0400

    Added a redirect for login filters.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,16 +1,37 @@
 import './App.css';
 import Register from './components/Register';
 import Login from './components/Login';
-import {Switch, Route} from 'react-router-dom';
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path='/register' component={Register}/>
-        <Route path='/login' component={Login}/>
-      </Switch>
-    </div>
-  );
+import Profile from './components/Profile';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {Component} from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+        }
+    }
+
+    handleIsLoggedIn() {
+        this.setState((previousState) => ({isLoggedIn: !previousState.isLoggedIn}))
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Switch>
+                    <Route path='/register'>
+                        {this.state.isLoggedIn ? <Redirect to="/profile"/> :  <Register/>}
+                    </Route>
+                    <Route path='/login'>
+                        {this.state.isLoggedIn ? <Redirect to="/profile"/> : <Login/>}
+                    </Route>
+                    <Route path="/profile" component={Profile} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
