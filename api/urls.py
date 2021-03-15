a4eddf60f8720f15821a758f3a0877dbdfef0ca4
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 11:57:30 -0400

    Added the events url.

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ app_name = "api_v1"
 urlpatterns = [
     # Short for ephemeris which
     # describes the various things about a celestial body.
-    path("ephem/", include("ephemeris.urls", namespace="ephemeris"))
+    path("ephem/", include("ephemeris.urls", namespace="ephemeris")),
+    path("events/", include("events.urls", namespace='events')),
 ]
