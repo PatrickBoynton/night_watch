eae0243695055c14722544cde8997ea08d92d2c9
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 19:55:30 -0400

    Created an event form.

---
+++ frontend/static/src/components/EventForm.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+class EventForm extends Component {
+
+    render() {
+        return (
+            <div>
+                EventForm
+            </div>
+        );
+    }
+
+}
+
+export default EventForm
