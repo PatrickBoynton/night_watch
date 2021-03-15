345ad74fdd3b6062d95874aca4f81ff1a0bd926f
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 11:57:12 -0400

    Changed the field names.

--- ephemeris/models.py
+++ ephemeris/models.py
@@ -4,9 +4,12 @@ from django.db import models
 # Create your models here.
 class Ephem(models.Model):
     name = models.CharField(max_length=40)
-    alt = models.FloatField()
-    az = models.FloatField()
+    rise_time = models.TimeField(null=True, blank=True)
+    set_time = models.TimeField(null=True, blank=True)
     is_visible = models.BooleanField()
     magnitude = models.FloatField()
     image = models.ImageField()
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
