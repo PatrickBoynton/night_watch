868994fe6e34c26bd507317aba4ffd5e6ef82fb8
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 14:35:07 -0400

    Created methods for uploading the form, and image.

--- frontend/static/src/components/EphemForm.js
+++ frontend/static/src/components/EphemForm.js
@@ -1,9 +1,61 @@
 import {Component} from 'react';
+import Cookies from 'js-cookie';
 
-class EphemForm extends Component{
+class EphemForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            rise_time: '',
+            set_time: '',
+            magnitude: 0,
+            image: null,
+            description: '',
+            preview: '',
+        };
+    }
+
+    handleInput(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleImage(e) {
+        let file = e.target.file[0];
+        this.setState({image: file});
+        let reader = new FileReader();
+        reader.onloadend = () => {
+            this.setState({
+                preview: reader.result
+            });
+        };
+        reader.readAsDataURL(file);
+    }
+
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        let formData = new FormData();
+        formData.append('image', this.state.image);
+        const options = {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': Cookies.get('csrftoken'),
+            },
+            body: formData
+        };
+
+        const response = await fetch('/api/v1/ephem/', options);
+        const data = await response.json();
+
+        console.log(data);
+
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={(e) => this.handleSubmit(e, this.state)}>
                 <label className="form-label" htmlFor="name">Name</label>
                 <input className="form-control" type="text" name="name"/>
                 <label className="form-label" htmlFor="rise_time">Rise Time</label>
@@ -15,11 +67,11 @@ class EphemForm extends Component{
                 <label className="form-label" htmlFor="image">Image</label>
                 <input className="form-control" type="file" name="image"/>
                 <label className="form-label" htmlFor="description">Description</label>
-                <textarea name="description" cols="30" rows="10"></textarea>
+                <textarea className="form-control" name="description" cols="30" rows="10"></textarea>
                 <button className="btn btn-primary" type="submit">Submit</button>
             </form>
         );
     }
 }
 
-export default EphemForm
+export default EphemForm;
