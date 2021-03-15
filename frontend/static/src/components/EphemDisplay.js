2be38325f08b06f1787480aaa7a4fb45c3af07fa
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 11:56:44 -0400

    Added a display and form.

---
+++ frontend/static/src/components/EphemDisplay.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class EphemDisplay extends Component{
+    render() {
+        return (
+            <div>
+                EphemDisplay
+            </div>
+        );
+    }
+}
+
+export default EphemDisplay
---
+++ frontend/static/src/components/EphemForm.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class EphemForm {
+    render() {
+        return (
+            <div>
+                EphemForm
+            </div>
+        );
+    }
+}
+
+export default EphemForm
