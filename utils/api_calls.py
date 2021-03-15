90d35ad19e7a847f1910e8ce7fd30041120afce4
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 09:38:59 -0400

    Playing around with the api.

--- utils/api_calls.py
+++ utils/api_calls.py
@@ -1,13 +1,14 @@
 from skyfield.api import N, W, load, wgs84
 
 planets = load('de421.bsp')
-earth, sun = planets['earth'], planets['sun']
+earth, sun, moon = planets['earth'], planets['sun'], planets['moon']
 ts = load.timescale()
 t = ts.now()
 
 greenville = earth + wgs84.latlon(34.8526 * N, 82.3940 * W)
-astrometric = greenville.at(t).observe(sun)
+astrometric = greenville.at(t).observe(moon)
 
 alt, az, d = astrometric.apparent().altaz()
 
 print(alt)
+print(az)
