4e65db49c1aa9d261ef1ca12efd617bd89dd7eb6
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 21:16:05 -0400

    Uncommented out time.

--- events/models.py
+++ events/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=60)
-    ephemeris = models.CharField(max_length=30)
-    time = models.TimeField()
-    description = models.TextField()
+    # ephemeris = models.CharField(max_length=30)
+    time = models.TimeField(null=True, blank=True)
+    # description = models.TextField()
 
     def __str__(self):
         return self.name
