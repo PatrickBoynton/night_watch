33645a9f258955918fed3039aa5a28fc638fbfd6
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 21:23:58 -0400

    Uncommented out description.

--- events/models.py
+++ events/models.py
@@ -6,7 +6,7 @@ class Event(models.Model):
     name = models.CharField(max_length=60)
     # ephemeris = models.CharField(max_length=30)
     time = models.TimeField(null=True, blank=True)
-    # description = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return self.name
