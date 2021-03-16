a5e33769905be7971f283d4b3041b7663ec4f3c3
PatrickBoynton <jpboynton3@gmail.com>
2021-03-16 16:59:22 -0400

    Updated the permissions.

--- ephemeris/views.py
+++ ephemeris/views.py
@@ -6,10 +6,12 @@ from .serializers import EphemSerializer
 
 # Create your views here.
 class EphemListView(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     queryset = Ephem.objects.all()
     serializer_class = EphemSerializer
 
 
 class EphemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     queryset = Ephem.objects.all()
     serializer_class = EphemSerializer
