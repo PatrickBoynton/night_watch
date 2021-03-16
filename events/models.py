ab92fd73db282b85ba00f3087be6dba6a4bb1cc3
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 21:41:19 -0400

    Uncommented out a field.

--- events/models.py
+++ events/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=60)
-    # ephemeris = models.CharField(max_length=30)
+    ephemeris = models.CharField(max_length=30, null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
