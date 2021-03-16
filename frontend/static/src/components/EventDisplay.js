c7030aebfb5a163cf178da23261bc5c51c5d0624
PatrickBoynton <jpboynton3@gmail.com>
2021-03-15 22:13:12 -0400

    Added the ability to display events.

--- frontend/static/src/components/EventDisplay.js
+++ frontend/static/src/components/EventDisplay.js
@@ -1,10 +1,36 @@
 import {Component} from 'react';
 
 class EventDisplay extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isEditMode: false,
+            isAdmin: true,
+        }
+    }
+
+    async componentDidMount() {
+        const response = await fetch("/api/v1/events/");
+        const data = await response.json();
+        console.log(data);
+        this.setState({data})
+    }
+
     render() {
+        const events = this.state.data?.map(event => <section key={event.id}>
+            <h2>{event.name}</h2>
+            <p>{event.ephemeris}</p>
+            <p>{event.time}</p>
+            <p>{event.description}</p>
+            <div className="button-group">
+                <button className="btn btn-primary">Edit</button> <button className="btn btn-danger">Delete</button>
+            </div>
+        </section>);
         return (
             <div>
-                EventDisplay
+                {events}
             </div>
         );
     }
