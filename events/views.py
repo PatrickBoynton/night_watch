fe4f2b4438e80e518176a1afbef04a6cacbe6bb1
PatrickBoynton <jpboynton3@gmail.com>
2021-03-16 16:59:18 -0400

    Updated the permissions.

--- events/views.py
+++ events/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Event
 from .serializers import EventSerializer
+from rest_framework import permissions
 
 
 # Create your views here.
 class EventView(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated, )
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
-    lookup_field = "pk"
+    permission_classes = (permissions.IsAuthenticated, )
     queryset = Event.objects.all()
     serializer_class = EventSerializer
