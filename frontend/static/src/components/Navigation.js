a32b1aabe2c345fd76a9ff0c8cc5ef6c1231177d
PatrickBoynton <jpboynton3@gmail.com>
2021-03-16 09:00:06 -0400

    created navigation.

--- frontend/static/src/components/Navigation.js
+++ frontend/static/src/components/Navigation.js
@@ -1,11 +1,20 @@
 import {Component} from 'react';
+import {NavLink} from 'react-router-dom';
 
 class Navigation extends Component{
     render() {
         return (
-            <div>
-                Navigation
-            </div>
+            <nav className="navbar container-fluid navbar-dark">
+                <NavLink to="/register">Register</NavLink>
+                <NavLink to="/login">Login</NavLink>
+                {/*TODO only when logged in*/}
+                <NavLink to="/profile">Profile</NavLink>
+                {/*TODO Only as admin.*/}
+                <NavLink to="/ephem/form">Add Planet</NavLink>
+                <NavLink to="/event/form">Add Event</NavLink>
+                <NavLink to="/celestial-list">Planets</NavLink>
+                <NavLink to="/events">Upcoming Events</NavLink>
+            </nav>
         );
     }
 }
