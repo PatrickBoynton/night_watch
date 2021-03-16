c223d48e587619c5d75cc15ad7412cfc09dbc895
PatrickBoynton <jpboynton3@gmail.com>
2021-03-16 08:43:01 -0400

    Added a navigation component.

---
+++ frontend/static/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class Navigation extends Component{
+    render() {
+        return (
+            <div>
+                Navigation
+            </div>
+        );
+    }
+}
+
+export default Navigation
