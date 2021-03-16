1b0fa5b17dffe9f93d2bdf710fe655c99d86f404
PatrickBoynton <jpboynton3@gmail.com>
2021-03-16 16:43:52 -0400

    Added an edit mode.

--- frontend/static/src/components/ephem/EphemList.js
+++ frontend/static/src/components/ephem/EphemList.js
@@ -5,13 +5,52 @@ class EphemList extends Component {
         super(props);
         this.state = {
             ephems: [],
+            isEditMode: false,
         };
+        this.handleEditMode = this.handleEditMode.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     async componentDidMount() {
         const response = await fetch('api/v1/ephem/');
         const data = await response.json();
         this.setState({ephems: data});
+    }
+
+    handleEditMode() {
+        this.setState({isEditMode: true});
+    }
+
+    handleSubmit() {
+        this.setState({isEditMode: false});
+    }
+
+    showForm(item) {
+        return <form action="" onSubmit={this.handleSubmit}>
+            <label className="form-label" htmlFor="image">Image</label>
+            <input className="form-control"
+                   type="file"
+                   name="image"/>
+
+            <label className="form-label" htmlFor="name">Name</label>
+            <input className="form-control"
+                   type="text"
+                   name="name"/>
+
+            <label className="form-label" htmlFor="rise_time">Rise Time</label>
+            <input className="form-control" type="text" name="rise_time"/>
+
+            <label className="form-label" htmlFor="set_time">Set Time</label>
+            <input className="form-control" type="text" name="set_time"/>
+
+            <label htmlFor="description" className="form-label">Description</label>
+            <textarea className="form-control" name="description" id="" cols="30" rows="10">
+
+            </textarea>
+
+            <button className="btn btn-success">Edit Planet</button>
+        </form>;
+
     }
 
     render() {
@@ -21,11 +60,19 @@ class EphemList extends Component {
                 <h2>{item.name}</h2>
                 <p>rise time: {item.rise_time}</p>
                 <p>set time: {item.set_time}</p>
+                <button onClick={this.handleEditMode} className="btn btn-primary">Edit</button>
+                <button className="btn btn-danger">Delete</button>
             </div>
         </section>);
         return (
             <div className="container">
-                {list}
+                {
+                    !this.state.isEditMode
+                        ?
+                        list
+                        :
+                        this.showForm()
+                }
             </div>
         );
     }
