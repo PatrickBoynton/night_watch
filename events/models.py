7579a65bfa795b78b22e408b967e9f5fe4a118ef
PatrickBoynton <jpboynton3@gmail.com>
2021-03-16 16:59:30 -0400

    Added a user field.

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
@@ -7,6 +8,8 @@ class Event(models.Model):
     ephemeris = models.CharField(max_length=30, null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
