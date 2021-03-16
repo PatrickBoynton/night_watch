169aff023a9c743fb02ed7f5d91b08bef27dc985
PatrickBoynton <jpboynton3@gmail.com>
2021-03-16 00:14:17 -0400

    Working on an edit form.

--- frontend/static/src/components/events/EventDisplay.js
+++ frontend/static/src/components/events/EventDisplay.js
@@ -1,7 +1,7 @@
 import {Component} from 'react';
 import Cookies from 'js-cookie';
 
-class EventDisplay extends Component{
+class EventDisplay extends Component {
 
     constructor(props) {
         super(props);
@@ -9,43 +9,84 @@ class EventDisplay extends Component{
             data: [],
             isEditMode: false,
             isAdmin: true,
-        }
+            name: '',
+            ephemeris: '',
+            time: '',
+        };
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleEditMode = this.handleEditMode.bind(this);
     }
 
-    handleDelete(event, id) {
-        event.preventDefault();
+    handleDelete(id) {
         const options = {
-            method: "DELETE",
+            method: 'DELETE',
             headers: {
-                "X-CSRFToken": Cookies.get("csrftoken")
+                'X-CSRFToken': Cookies.get('csrftoken')
             }
-        }
-        fetch(`/api/v1/events/${id}/`, options)
+        };
+        fetch(`/api/v1/events/${id}/`, options);
     }
 
     async componentDidMount() {
-        const response = await fetch("/api/v1/events/");
+        const response = await fetch('/api/v1/events/');
         const data = await response.json();
         console.log(data);
-        this.setState({data})
+        this.setState({data});
+    }
+
+    showForm() {
+        return <form action="">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" value={this.state.name}/>
+            <label htmlFor="ephemeris">Target</label>
+            <input type="text" name="ephemeris" value={this.state.ephemeris}/>
+            <label htmlFor="time">Time</label>
+            <input type="text" name="time" value={this.state.time}/>
+            <label htmlFor="description">Description</label>
+            <textarea name="description" value={this.state.description} cols="30" rows="10">
+            </textarea>
+            <button type="submit">Edit</button>
+        </form>;
+    }
+
+    handleEditMode(e) {
+        this.setState((previousState) => ({isEditMode: !previousState.isEditMode}));
+        this.setState({name: e.name,
+                             ephemeris: e.ephemeris, time: e.time})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
     }
 
     render() {
-        const events = this.state.data?.map(event => <section key={event.id}>
-            <h2>{event.name}</h2>
-            <p>{event.ephemeris}</p>
-            <p>{event.time}</p>
-            <p>{event.description}</p>
-            <div className="button-group">
-                <button className="btn btn-primary">Edit</button> <button onClick={(e) => this.handleDelete(e, event.id)} className="btn btn-danger">Delete</button>
-            </div>
+        let events = this.state.data?.map(event => <section key={event.id}>
+            <>
+                <h2>{event.name}</h2>
+                <p>{event.ephemeris}</p>
+                <p>{event.time}</p>
+                <p>{event.description}</p>
+                <div className="button-group">
+                    <button onClick={() => this.handleEditMode(event)} className="btn btn-primary">Edit</button>
+                    <button onClick={() => this.handleDelete(event.id)} className="btn btn-danger">Delete
+                    </button>
+                </div>
+            </>
         </section>);
         return (
-            <div>
-                {events}
-            </div>
+            <>
+                {
+                    !this.state.isEditMode
+                        ?
+                        events
+                        :
+                        this.showForm()
+                }
+            </>
         );
     }
+
 }
 
-export default EventDisplay
+export default EventDisplay;
