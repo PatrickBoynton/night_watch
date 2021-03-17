d66fd36e81b2e64cd1084b509e46156a45368ed5
PatrickBoynton <jpboynton3@gmail.com>
2021-03-17 15:32:49 -0400

    Created crud views for the profile section.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -7,5 +7,22 @@ from .serializers import ProfileSerializer
 
 # Create your views here.
 class ProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProfileCreateView(generics.CreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProfileUpdateView(generics.RetrieveUpdateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProfileDeleteView(generics.RetrieveDestroyAPIView):
+    permission_classes = (permissions.IsAdminUser,)
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
