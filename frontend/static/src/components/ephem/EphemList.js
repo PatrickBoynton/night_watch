a218685b7813a955029f43c928b7ab5801d51d17
PatrickBoynton <jpboynton3@gmail.com>
2021-03-17 10:34:49 -0400

    Commented out the admin section. Styling with bootstrap.

--- frontend/static/src/components/ephem/EphemList.js
+++ frontend/static/src/components/ephem/EphemList.js
@@ -27,52 +27,53 @@ class EphemList extends Component {
         this.setState({isEditMode: false});
     }
 
-    showForm(item) {
-        return <form action="" onSubmit={this.handleSubmit}>
-            <label className="form-label" htmlFor="image">Image</label>
-            <input className="form-control"
-                   type="file"
-                   name="image"/>
-
-            <label className="form-label" htmlFor="name">Name</label>
-            <input className="form-control"
-                   type="text"
-                   name="name"/>
-
-            <label className="form-label" htmlFor="rise_time">Rise Time</label>
-            <input className="form-control" type="text" name="rise_time"/>
-
-            <label className="form-label" htmlFor="set_time">Set Time</label>
-            <input className="form-control" type="text" name="set_time"/>
-
-            <label htmlFor="description" className="form-label">Description</label>
-            <textarea className="form-control" name="description" id="" cols="30" rows="10">
-
-            </textarea>
-
-            <button className="btn btn-success">Edit Planet</button>
-        </form>;
-
-    }
+    // showForm(item) {
+    //     return <form action="" onSubmit={this.handleSubmit}>
+    //         <label className="form-label" htmlFor="image">Image</label>
+    //         <input className="form-control"
+    //                type="file"
+    //                name="image"/>
+    //
+    //         <label className="form-label" htmlFor="name">Name</label>
+    //         <input className="form-control"
+    //                type="text"
+    //                name="name"/>
+    //
+    //         <label className="form-label" htmlFor="rise_time">Rise Time</label>
+    //         <input className="form-control" type="text" name="rise_time"/>
+    //
+    //         <label className="form-label" htmlFor="set_time">Set Time</label>
+    //         <input className="form-control" type="text" name="set_time"/>
+    //
+    //         <label htmlFor="description" className="form-label">Description</label>
+    //         <textarea className="form-control" name="description" id="" cols="30" rows="10">
+    //
+    //         </textarea>
+    //
+    //         <button className="btn btn-success">Edit Planet</button>
+    //     </form>;
+    //
+    // }
 
     render() {
-        const list = this.state.ephems.map(item => <section key={item.id} className="row">
-            <div className="col">
-                <img src={item.image} alt="A planet, star or satellite."/>
-                <h2>{item.name}</h2>
+        const list = this.state.ephems.map(item => <section key={item.id} className="container">
+            <div className="card">
+                <div className="card-header">
+                    <img src={item.image} alt="A planet, star or satellite."/>
+                    <h2>{item.name}</h2>
+                </div>
                 <p>rise time: {item.rise_time}</p>
                 <p>set time: {item.set_time}</p>
                 {
                     this.state.isAdmin
                         ?
-                        <>
+                        <div className="card-footer">
                             <button onClick={this.handleEditMode} className="btn btn-primary">Edit</button>
                             <button className="btn btn-danger">Delete</button>
-                        </>
+                        </div>
                         :
                         null
                 }
-
             </div>
         </section>);
         return (
