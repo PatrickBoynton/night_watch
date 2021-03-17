93dd4c55ee3b417f774ed4bd7272a962fad613e3
PatrickBoynton <jpboynton3@gmail.com>
2021-03-17 16:26:48 -0400

    Added querysets.

--- events/views.py
+++ events/views.py
@@ -7,12 +7,20 @@ from rest_framework import permissions
 
 # Create your views here.
 class EventView(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Event.objects.filter(user=user)
 
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Event.objects.filter(user=user)
