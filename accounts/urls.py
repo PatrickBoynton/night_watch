1f22488b8775a402090d1574df220b869e5180a2
PatrickBoynton <jpboynton3@gmail.com>
2021-03-17 00:02:29 -0400

    Temporarily removed this path.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import ProfileListView, ProfileDetailView
+from .views import ProfileListView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("", ProfileListView.as_view()),
-    path("<int:id>/", ProfileDetailView.as_view())
 ]
