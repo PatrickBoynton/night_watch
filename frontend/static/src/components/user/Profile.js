2b38bf873f40f915cbaca4ccedd358924c9fd0f9
PatrickBoynton <jpboynton3@gmail.com>
2021-03-16 21:05:57 -0400

    Working on adding the logged in user's information to the profile.

--- frontend/static/src/components/user/Profile.js
+++ frontend/static/src/components/user/Profile.js
@@ -2,6 +2,18 @@ import {Component} from 'react';
 import Cookies from 'js-cookie';
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    async componentDidMount() {
+        const response = await fetch("/rest-auth/user/");
+        const data = await response.json();
+        console.log(data);
+    }
+
 
     handleLogout() {
         Cookies.remove('Authorization');
@@ -9,13 +21,17 @@ class Profile extends Component {
 
     render() {
         return (
-            <>
-                <h1>Username</h1>
-                <img src="#" alt="user"/>
-                <p>About me</p>
-                <p>My equipment</p>
+            <div className="card">
+                <div className="card-header">
+                    <h1>Username</h1>
+                    <img src="#" alt="user"/>
+                    <p>About me</p>
+                </div>
+                <div className="card-footer">
+                    <p>My equipment</p>
                 <button onClick={this.handleLogout} className="btn btn-danger">Logout</button>
-            </>
+                </div>
+            </div>
         );
     }
 }
