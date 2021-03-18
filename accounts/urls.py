23660617f882c6b6099dea061f2ab2a20d79319f
PatrickBoynton <jpboynton3@gmail.com>
2021-03-18 15:49:11 -0400

    Fixed issues with urls.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,8 @@ app_name = "accounts"
 
 urlpatterns = [
     path('', views.ProfileListView.as_view()),
-    path('<int:pk>', views.ProfileDetailView.as_view()),
+    path('<int:pk>/', views.ProfileDetailView.as_view()),
     path('create/', views.ProfileCreateView.as_view()),
-    path('update/<int:pk>', views.ProfileUpdateView.as_view()),
-    path('delete/<int:pk>', views.ProfileDeleteView)
+    path('update/<int:pk>/', views.ProfileUpdateView.as_view()),
+    path('delete/<int:pk>/', views.ProfileDeleteView.as_view()),
 ]
