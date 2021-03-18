a1fe5e911b9db90086091d5bd38357760767dcb9
PatrickBoynton <jpboynton3@gmail.com>
2021-03-18 14:15:17 -0400

    Added a perform create.

--- accounts/views.py
+++ accounts/views.py
@@ -24,6 +24,9 @@ class ProfileCreateView(generics.CreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class ProfileUpdateView(generics.RetrieveUpdateAPIView):
     permission_classes = (permissions.IsAuthenticated,)
