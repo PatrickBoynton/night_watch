78b2b9f76f8df2d66e1f06333a68c1ec7c43491d
PatrickBoynton <jpboynton3@gmail.com>
2021-03-17 22:20:15 -0400

    Added a trailing comma.

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,6 @@ from .views import IndexView
 app_name = 'frontend'
 
 urlpatterns = [
-    path('<path:resource>', IndexView.as_view()),
+    path('<path:resource>/', IndexView.as_view()),
     path('', IndexView.as_view(), name='index'),
 ]
