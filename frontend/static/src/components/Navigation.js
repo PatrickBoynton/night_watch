b99d719de8dcc6c2b2cc478dd37ace8940e029e9
PatrickBoynton <jpboynton3@gmail.com>
2021-03-18 10:53:23 -0400

    Added conditional rendering.

--- frontend/static/src/components/Navigation.js
+++ frontend/static/src/components/Navigation.js
@@ -12,12 +12,24 @@ class Navigation extends Component {
     render() {
         return (
             <nav className="navbar container-fluid navbar-dark">
-                <NavLink to="/register">Register</NavLink>
-                <NavLink to="/login">Login</NavLink>
-                {/*TODO only when logged in*/}
-                <NavLink to="/profile">Profile</NavLink>
+                {
+                    this.props.isLoggedIn
+                        ?
+                        <NavLink to="/profile">Profile</NavLink>
+                        :
+                        <>
+                            <NavLink to="/register">Register</NavLink>
+                            <NavLink to="/login">Login</NavLink>
+                        </>
+                }
                 {/*TODO Only as admin.*/}
-                {/*<NavLink to="/ephem/form">Add Planet</NavLink>*/}
+                {
+                    this.state.isAdmin
+                        ?
+                        <NavLink to="/ephem/form">Add Planet</NavLink>
+                        :
+                        null
+                }
                 <NavLink to="/event/form">Add Event</NavLink>
                 <NavLink to="/celestial-list">Planets</NavLink>
                 <NavLink to="/events">Upcoming Events</NavLink>
