89602120e61acb72acf6761ac2225e25a66499e4
PatrickBoynton <jpboynton3@gmail.com>
2021-03-18 14:14:31 -0400

    Added a line.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Profile
+from .models import Profile, CustomUser
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Profile
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = CustomUser
         fields = '__all__'
