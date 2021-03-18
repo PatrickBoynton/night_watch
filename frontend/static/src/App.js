0991815506c6fd174266df0146c24a2460e0c51f
PatrickBoynton <jpboynton3@gmail.com>
2021-03-18 16:45:48 -0400

    Formatted, and added the new routes.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -12,12 +12,13 @@ import EventDisplay from './components/events/EventDisplay';
 import Navigation from './components/Navigation';
 import Cookies from 'js-cookie';
 import ProfileForm from './components/user/ProfileForm';
+import Home from './components/pages/Home';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLoggedIn: !!Cookies.get("Authorization"),
+            isLoggedIn: !!Cookies.get('Authorization'),
             isAdmin: false,
         };
         this.handleIsLoggedIn = this.handleIsLoggedIn.bind(this);
@@ -32,13 +33,18 @@ class App extends Component {
             <div className="App">
                 <Navigation isLoggedIn={this.state.isLoggedIn}/>
                 <Switch>
+                    <Route path='/home' component={Home}/>
+                    <Route path='/about'/>
+                    <Route path='/glossary'/>
                     <Route path='/register' component={Register}/>
-                    <Route path='/login' render={(props) => (<Login {...props} isLoggedIn={this.state.isLoggedIn} handleIsLoggedIn={this.handleIsLoggedIn} />)} />
-                    <Route path="/profile" render={(props) => (<Profile {...props} handleIsLoggedIn={this.handleIsLoggedIn} />)} />
+                    <Route path='/login' render={(props) => (<Login {...props} isLoggedIn={this.state.isLoggedIn}
+                                                                    handleIsLoggedIn={this.handleIsLoggedIn}/>)}/>
+                    <Route path="/profile"
+                           render={(props) => (<Profile {...props} handleIsLoggedIn={this.handleIsLoggedIn}/>)}/>
                     <Route path="/create-profile" component={ProfileForm}/>
                     <Route path="/ephem/form" component={EphemForm}/>
-                    <Route path="/event/form"  component={EventForm}/>
-                    <Route path="/display" component={EphemDisplay} />
+                    <Route path="/event/form" component={EventForm}/>
+                    <Route path="/display" component={EphemDisplay}/>
                     <Route path="/celestial-list" component={EphemList}/>
                     <Route path="/events" component={EventDisplay}/>
                 </Switch>
