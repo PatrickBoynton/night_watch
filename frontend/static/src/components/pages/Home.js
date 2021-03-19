1779f47a8343ea39477b0bc64e81f7841f891720
PatrickBoynton <jpboynton3@gmail.com>
2021-03-18 23:27:23 -0400

    Added basic filler text.

--- frontend/static/src/components/pages/Home.js
+++ frontend/static/src/components/pages/Home.js
@@ -3,9 +3,10 @@ import {Component} from 'react';
 class Home extends Component{
     render() {
         return (
-            <div>
-                Home
-            </div>
+            <>
+                <h2>Welcome to Night Watch</h2>
+                <p>Night Watch is a simple astronomy planning app, register or login to view more!</p>
+            </>
         );
     }
 }
