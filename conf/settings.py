7a92d1089d3f01f7fb07bd7ccf389caf51dd31ab
PatrickBoynton <jpboynton3@gmail.com>
2021-03-20 14:28:08 -0400

    Added a missing part to settings.

--- conf/settings.py
+++ conf/settings.py
@@ -151,6 +151,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES' : [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ]
+}
+REST_AUTH_SERIALIZERS = {
+    'TOKEN_SERIALIZER': 'accounts.serializers.TokenSerializer'
 }
