9ac04b730941f0546c1a99a2729e01808a396c1a
PatrickBoynton <jpboynton3@gmail.com>
2021-03-20 14:27:51 -0400

    Still trying to associate a user with their profile.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,29 @@
 from rest_framework import serializers
 from rest_auth.models import TokenModel
-from .models import Profile
+from .models import Profile, CustomUser
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    date = serializers.ReadOnlyField(source='user.date_joined')
 
     class Meta:
         model = Profile
         fields = '__all__'
 
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    date = serializers.ReadOnlyField(source='user.date_joined')
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+
 class TokenSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    profile = serializers.ReadOnlyField(source='CustomUser.profile')
 
     class Meta:
         model = TokenModel
