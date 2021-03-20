cfacb1f779f816d653492a4a07eefeccb341d721
PatrickBoynton <jpboynton3@gmail.com>
2021-03-20 17:58:29 -0400

    Working on setting up twilio.

--- broadcast/views.py
+++ broadcast/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse
+from twilio.rest import Client
+
 
 # Create your views here.
+def broadcast_sms(request):
+    message_to_broadcast = 'Hello world from Twilio back end!'
+
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+
+    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+        if recipient:
+            client.messages.create(to=recipient,
+                                   from_=settings.TWILIO_NUMBER,
+                                   body=message_to_broadcast)
+
+    return HttpResponse("Message sent!", 200)
