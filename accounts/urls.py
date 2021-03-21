ce464d4201be228f61abafd6e65b4dabf6d1e21c
PatrickBoynton <jpboynton3@gmail.com>
2021-03-21 14:17:09 -0400

    Changed the url.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path('', views.ProfileListView.as_view()),
-    path('<int:pk>/', views.ProfileDetailView.as_view()),
+    path('details/', views.ProfileDetailView.as_view()),
     path('create/', views.ProfileCreateView.as_view()),
     path('update/<int:pk>/', views.ProfileUpdateView.as_view()),
     path('delete/<int:pk>/', views.ProfileDeleteView.as_view()),
