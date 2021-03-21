738f98e093117d82abc661350ed908e596b1b033
PatrickBoynton <jpboynton3@gmail.com>
2021-03-21 14:16:59 -0400

    Fetched and displayed the current users profile.

--- frontend/static/src/components/user/Profile.js
+++ frontend/static/src/components/user/Profile.js
@@ -5,14 +5,21 @@ import {Link, Redirect} from 'react-router-dom';
 class Profile extends Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            user: '',
+            profile_picture:'',
+            about_me: '',
+            my_equipment: []
+        }
         this.handleLogout = this.handleLogout.bind(this);
     }
 
     async componentDidMount() {
-        const response = await fetch('/rest-auth/user/');
+        const response = await fetch('api/v1/profiles/details/')
         const data = await response.json();
-        console.log(data);
+        this.setState({user: data.user,
+                            profile_picture: data.profile_picture,
+                            about_me: data.about_me})
     }
 
 
@@ -34,19 +41,19 @@ class Profile extends Component {
     }
 
     render() {
+        const equipment = this.state.equipment?.map(item => <p>{item}</p>)
         return (
             <div className="container test">
                 <div className="left-boarder">
-                    <h1>Username</h1>
-                    <img src="#" alt="username"/>
-                    <p>Telescope, binoculars, eyes</p>
+                    <h1>{this.state.user}</h1>
+                    <img src={this.state.profile_picture} alt="username"/>
+                    <p>{equipment}</p>
                     <p>If you haven't yet created a profile, why not <Link to="/create-profile">Create a
                         Profile</Link></p>
                     <button onClick={this.handleLogout} className="btn btn-danger">Logout</button>
                 </div>
                 <div className="right-boarder">
-                    <p className="about-text" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque deleniti error laudantium non,
-                        quasi ratione recusandae reprehenderit tempore ullam vel?</p>
+                    <p className="about-text" >{this.state.about_me}</p>
                 </div>
             </div>
         );
