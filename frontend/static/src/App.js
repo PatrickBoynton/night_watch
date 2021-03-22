a9d5f5be263a5df52a505b17d46baeab0415f136
PatrickBoynton <jpboynton3@gmail.com>
2021-03-22 15:12:55 -0400

    Moved the profile call to the app.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -23,9 +23,31 @@ class App extends Component {
         this.state = {
             isLoggedIn: !!Cookies.get('Authorization'),
             isAdmin: false,
+            user: {
+                user: '',
+                profile_picture: '',
+                about_me: '',
+                my_equipment: []
+            }
         };
         this.handleIsLoggedIn = this.handleIsLoggedIn.bind(this);
     }
+
+    async componentDidMount() {
+        const response = await fetch('api/v1/profiles/details');
+        const data = await response.json();
+
+
+        this.setState({
+            user: {
+                user: data.user,
+                profile_picture: data.profile_picture,
+                about_me: data.about_me,
+                my_equipment: data.my_equipment
+            }
+        });
+    }
+
 
     handleIsLoggedIn() {
         this.setState((previousState) => ({isLoggedIn: !previousState.isLoggedIn}));
@@ -37,20 +59,21 @@ class App extends Component {
                 <Navigation isLoggedIn={this.state.isLoggedIn}/>
                 <Switch>
                     <Route path='/home' component={Home}/>
-                    <Route path='/about' component={About} />
+                    <Route path='/about' component={About}/>
                     <Route path='/glossary' component={Glossary}/>
                     <Route path='/register' component={Register}/>
                     <Route path='/login' render={(props) => (<Login {...props} isLoggedIn={this.state.isLoggedIn}
                                                                     handleIsLoggedIn={this.handleIsLoggedIn}/>)}/>
                     <Route path="/profile"
-                           render={(props) => (<Profile {...props} handleIsLoggedIn={this.handleIsLoggedIn}/>)}/>
+                           render={(props) => (<Profile {...props} user={this.state.user}
+                                                        handleIsLoggedIn={this.handleIsLoggedIn}/>)}/>
                     <Route path="/create-profile" component={ProfileForm}/>
                     <Route path="/ephem/form" component={EphemForm}/>
                     <Route path="/event/form" component={EventForm}/>
                     <Route path="/display" component={EphemDisplay}/>
                     <Route path="/celestial-list" component={EphemList}/>
                     <Route path="/events" component={EventDisplay}/>
-                    <Route path='/sms-form' component={SMSForm} />
+                    <Route path='/sms-form' component={SMSForm}/>
                 </Switch>
             </div>
         );
