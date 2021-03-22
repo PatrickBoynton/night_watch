b5b07e14be8823a0a7b6f26790374f840c504c11
PatrickBoynton <jpboynton3@gmail.com>
2021-03-22 15:20:55 -0400

    Changed state to props.

--- frontend/static/src/components/user/Profile.js
+++ frontend/static/src/components/user/Profile.js
@@ -5,25 +5,8 @@ import {Link, Redirect} from 'react-router-dom';
 class Profile extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            user: '',
-            profile_picture:'',
-            about_me: '',
-            my_equipment: []
-        }
         this.handleLogout = this.handleLogout.bind(this);
     }
-
-    async componentDidMount() {
-        const response = await fetch('api/v1/profiles/details/')
-        const data = await response.json();
-        console.log(data);
-        this.setState({user: data.user,
-                            profile_picture: data.profile_picture,
-                            my_equipment: data.my_equipment,
-                            about_me: data.about_me})
-    }
-
 
     async handleLogout() {
 
@@ -43,20 +26,19 @@ class Profile extends Component {
     }
 
     render() {
-        const equipment = this.state.my_equipment?.map(item => <p>{item}</p>)
-        console.log(equipment);
+        const equipment = this.props.user.my_equipment?.map(item => <p key={item.id}>{item}</p>)
         return (
             <div className="container test">
                 <div className="left-boarder">
-                    <h1>{this.state.user}</h1>
-                    <img className="profile-image" src={this.state.profile_picture} alt="username"/>
-                    <p>{equipment}</p>
+                    <h1>{this.props.user.user}</h1>
+                    <img className="profile-image" src={this.props.user.profile_picture} alt="username"/>
+                    <div>{equipment}</div>
                     <p>If you haven't yet created a profile, why not <Link to="/create-profile">Create a
                         Profile</Link></p>
                     <button onClick={this.handleLogout} className="btn btn-danger">Logout</button>
                 </div>
                 <div className="right-boarder">
-                    <p className="about-text" >{this.state.about_me}</p>
+                    <p className="about-text" >{this.props.user.about_me}</p>
                 </div>
             </div>
         );
