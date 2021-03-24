a3c56d55950b05cd224b4ad2050dd29f3f5d2fb3
PatrickBoynton <jpboynton3@gmail.com>
2021-03-24 01:04:39 -0400

    Working on logout in the navbar.

--- frontend/static/src/components/Navigation.js
+++ frontend/static/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import {Component} from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, Redirect} from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 class Navigation extends Component {
     constructor(props) {
@@ -7,6 +8,25 @@ class Navigation extends Component {
         this.state = {
             isAdmin: false,
         };
+
+
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    async handleLogout() {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'Application/Json',
+                'X-CSRFToken': Cookies.get('csrftoken'),
+                'Authorization': Cookies.get('Authorization'),
+            }
+        };
+        await fetch('/rest-auth/logout/', options);
+        Cookies.remove('csrftoken');
+        Cookies.remove('Authorization');
+        this.state.history.push('/login');
+        return <Redirect to='/login'/>;
     }
 
     render() {
@@ -16,18 +36,18 @@ class Navigation extends Component {
                     this.props.isLoggedIn
                         ?
                         <>
-                            <NavLink className="navbar-brand" to='/home' >Night Watch</NavLink>
+                            <NavLink className="navbar-brand" to='/home'>Night Watch</NavLink>
                             <NavLink to="/profile">Profile</NavLink>
                             <NavLink to="/event/form">Add Event</NavLink>
                             <NavLink to="/celestial-list">Celestial Events</NavLink>
                             <NavLink to="/events">Upcoming Events</NavLink>
-                            <NavLink to='/glossary' >Glossary</NavLink>
-                            <NavLink to='/about' >About</NavLink>
+                            <NavLink to='/glossary'>Glossary</NavLink>
+                            <NavLink to='/logout' onClick={this.handleLogout}>Logout</NavLink>
                             {/*<NavLink>Logout</NavLink>*/}
                         </>
                         :
                         <>
-                            <NavLink to='/home' >Home</NavLink>
+                            <NavLink to='/home'>Home</NavLink>
                             <NavLink to="/register">Register</NavLink>
                             <NavLink to="/login">Login</NavLink>
                         </>
