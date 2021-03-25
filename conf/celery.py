1ab848c967cbb2ce84c2bc73cc1ec12256598492
PatrickBoynton <jpboynton3@gmail.com>
2021-03-25 13:37:24 -0400

    Added a working schedule.

--- conf/celery.py
+++ conf/celery.py
@@ -9,21 +9,16 @@ app.config_from_object('django.conf:settings', namespace='Celery')
 
 app.autodiscover_tasks()
 
+app.conf.beat_schedule = {
+    'print-hello-world': {
+        'task': 'ephemeris.tasks.hello',
+        'schedule': 5.0,
+    }
+}
 
-# app.conf.beat_schedule = {
-#     'print-hello-world': {
-#         'task': 'ephemeris.say_hello',
-#         'schedule': 5.0
-#     }
-# }
-
-@app.on_after_configure.connect
-def setup_periodic_taasks(sender, **kwargs):
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
-
-    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+app.conf.timezone = 'UTC'
 
 
-@app.task
-def test(arg):
-    print(arg)
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
