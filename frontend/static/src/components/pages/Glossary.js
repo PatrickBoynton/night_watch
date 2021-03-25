861301074aa5ab5bdc949c7442c126541c7fcf95
PatrickBoynton <jpboynton3@gmail.com>
2021-03-24 20:14:27 -0400

    Working on adding content.

--- frontend/static/src/components/pages/Glossary.js
+++ frontend/static/src/components/pages/Glossary.js
@@ -1,10 +1,24 @@
 import {Component} from 'react';
 
-class Glossary extends Component{
+class Glossary extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            terms: [
+                {id: 1, term: 'Look again at that dot. That\'s here. That\'s home. That\'s us. On it everyone you love, everyone you know, everyone you ever heard of, every human being who ever was, lived out their lives. The aggregate of our joy and suffering, thousands of confident religions, ideologies, and economic doctrines, every hunter and forager, every hero and coward, every creator and destroyer of civilization, every king and peasant, every young couple in love, every mother and father, hopeful child, inventor and explorer, every teacher of morals, every corrupt politician, every "superstar," every "supreme leader," every saint and sinner in the history of our species lived there--on a mote of dust suspended in a sunbeam.'},
+                {id: 2, term: 'It is for these reasons that I regard the decision last year to shift our efforts in space from low to high gear as among the most important decisions that will be made during my incumbency in the office of the Presidency.' },
+                {id: 3, term: 'Spaceflight will never tolerate carelessness, incapacity, and neglect. Somewhere, somehow, we screwed up. It could have been in design, build, or test. Whatever it was, we should have caught it. We were too gung ho about the schedule and we locked out all of the problems we saw each day in our work'},
+                {id: 4, term: ' believe that space travel will one day become as common as airline travel is today. I’m convinced, however, that the true future of space travel does not lie with government agencies — NASA is still obsessed with the idea that the primary purpose of the space program is science — but real progress will come from private companies competing to provide the ultimate adventure ride, and NASA will receive the trickle-down benefits.'},
+             ]
+        };
+    }
+
     render() {
+        const terms = this.state.terms?.map(x => console.log(x.term));
         return (
             <div>
-                Glossary
+                {/*{terms}*/}
             </div>
         );
     }
