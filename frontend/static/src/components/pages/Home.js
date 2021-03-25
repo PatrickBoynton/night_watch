336e5358bee314016408cda1e27b09d3d30811b7
PatrickBoynton <jpboynton3@gmail.com>
2021-03-24 20:14:51 -0400

    Added lorem text.

--- frontend/static/src/components/pages/Home.js
+++ frontend/static/src/components/pages/Home.js
@@ -1,14 +1,23 @@
 import {Component} from 'react';
 
-class Home extends Component{
+class Home extends Component {
     render() {
         return (
             <>
                 <h2>Welcome to Night Watch</h2>
-                <p>Night Watch is a simple astronomy planning app, register or login to view more!</p>
+                <p>The view of the earth from the moon fascinated me - a small disk, 240,000 mniles away. It was hard to
+                    think that that little thing held so many problems, so many frustrations. Raging nationalistic
+                    interests, famines, wars, pestilence don't show from that distance. I'm convinced that some wayward
+                    stranger in a space-craft, coming from some other part of the heavens, could look at earth and never
+                    know that it was inhabited at all. But the samw wayward stranger would certainly know instinctively
+                    that if the earth were inhabited, then the destinies of all who lived on it must inevitably be
+                    interwoven and joined. We are one hunk of ground, water, air, clouds, floating around in space. From
+                    out there it really is 'one world'.
+
+                </p>
             </>
         );
     }
 }
 
-export default Home
+export default Home;
