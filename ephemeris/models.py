adff964b80870c83045ec5a643010e560f124f7e
PatrickBoynton <jpboynton3@gmail.com>
2021-03-26 16:14:22 -0400

    Removed the rise time and set time.

--- ephemeris/models.py
+++ ephemeris/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Ephem(models.Model):
     name = models.CharField(max_length=40)
-    rise_time = models.TimeField(null=True, blank=True)
-    set_time = models.TimeField(null=True, blank=True)
+    # rise_time = models.TimeField(null=True, blank=True)
+    # set_time = models.TimeField(null=True, blank=True)
     is_visible = models.BooleanField()
     magnitude = models.FloatField()
     image = models.ImageField(upload_to='ephemeris/')
