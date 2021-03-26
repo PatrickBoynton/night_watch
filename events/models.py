e10d5c71b4c97714cd66cf4f3074d1ee9aa2821d
PatrickBoynton <jpboynton3@gmail.com>
2021-03-26 00:03:06 -0400

    Updated the model.

--- events/models.py
+++ events/models.py
@@ -31,6 +31,7 @@ class Event(models.Model):
                                    max_length=50,
                                    default='recurring')
     phone = models.CharField(max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to='events/', null=True, blank=True)
 
     def __str__(self):
         return self.name
