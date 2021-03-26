33bf59b1c3f07589bb5432b1c7dcf5030059d5ad
PatrickBoynton <jpboynton3@gmail.com>
2021-03-26 14:42:01 -0400

    Removed the TODO.

--- conf/settings.py
+++ conf/settings.py
@@ -153,7 +153,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Celery
-# TODO change to heroku url.
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
