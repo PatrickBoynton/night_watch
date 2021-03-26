779a1fe742de6ffc53a657046f40903b86483e25
PatrickBoynton <jpboynton3@gmail.com>
2021-03-25 23:54:26 -0400

    Playing around with scheduled tasks.

--- ephemeris/tasks.py
+++ ephemeris/tasks.py
@@ -3,4 +3,13 @@ from celery import shared_task
 
 @shared_task
 def hello():
-    print('Hello world')
+    print('Hello World!')
+
+
+@shared_task
+def send_text():
+    print('Sent Text!')
+
+@shared_task
+def find_stuff():
+    print('Testing!')
