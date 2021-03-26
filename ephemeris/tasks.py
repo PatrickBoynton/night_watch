7221159ece8f62d2fbe88149687ce68a9fbf56be
PatrickBoynton <jpboynton3@gmail.com>
2021-03-26 17:03:45 -0400

    Removed commented code. Moved the db stuff.

--- ephemeris/tasks.py
+++ ephemeris/tasks.py
@@ -14,40 +14,18 @@ saturn = Ephem.objects.get(name='Saturn')
 uranus = Ephem.objects.get(name='Uranus')
 neptune = Ephem.objects.get(name='Neptune')
 
-planets = [sun, mercury, venus, moon, mars, jupiter, saturn, uranus, neptune]
-
-# ts = load.timescale()
-# t0 = ts.utc(2021, 3, 18)
-# t1 = ts.utc(2021, 3, 19)
-# eph = load('de421.bsp')
-#
-# greenville = wgs84.latlon(34.8526 * N, 82.3940 * W, elevation_m=299.923)
-# planet = eph['sun']
-#
-# f = risings_and_settings(eph, planet, greenville)
-# tz = timezone('US/Eastern')
-#
-# for t, updown in zip(*find_discrete(t0, t1, f)):
-#     print(t.astimezone(tz).strftime('%a %d %H:%M'), 'EST',
-#           'rises' if updown else 'sets')
-
-
-# sun.rise_time = '03:04:05'
-# sun.save()
-
 eph = load('de421.bsp')
-planet = eph['sun']
+planet = eph['mars']
 
 
 @app.shared_task
 def get_ephem_times():
     ts = load.timescale()
+
     t0 = ts.utc(2021, 3, 26)
     t1 = ts.utc(2021, 3, 27)
 
-    results = []
-    # sun.rise_time = results[0]
-    # sun.save()
+    dates = []
 
     greenville = wgs84.latlon(34.8526 * N, 82.3940 * W, elevation_m=299.923)
     global planet
@@ -56,6 +34,8 @@ def get_ephem_times():
     tz = timezone('US/Eastern')
 
     for t, updown in zip(*find_discrete(t0, t1, f)):
-        results.append(t.astimezone(tz).strftime('%y-%m-%d %H:%M'))
+        dates.append(t.astimezone(tz).strftime('%Y-%m-%d %H:%M'))
 
-    print(results[0])
+    sun.rise_time = dates[0]
+    sun.set_time = dates[1]
+    sun.save()
