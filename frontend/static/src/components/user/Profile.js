3632fc2b5fce44f25de029ceae07231b4bae94c9
PatrickBoynton <jpboynton3@gmail.com>
2021-03-28 11:00:54 -0400

    Changed the profile look.

--- frontend/static/src/components/user/Profile.js
+++ frontend/static/src/components/user/Profile.js
@@ -27,18 +27,29 @@ class Profile extends Component {
 
     render() {
         return (
-            <div className="container test">
-                <div className="left-boarder">
-                    <h1>{this.props.user.user}</h1>
-                    <img className="profile-image" src={this.props.user.profile_picture} alt="username"/>
-                    <p>If you haven't yet created a profile, why not <Link to="/create-profile">Create a
-                        Profile</Link></p>
-                    <button onClick={this.handleLogout} className="btn btn-danger">Logout</button>
-                </div>
-                <div className="right-boarder">
-                    <p className="about-text" >{this.props.user.about_me}</p>
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <h1>{this.props.user.user}</h1>
+                        <img  style={{height: '200px', width: '200px'}} className="rounded-circle" src={this.props.user.profile_picture} alt="username"/>
+                        <p>{this.props.user.about_me}</p>
+                        <p>If you haven't created a profile, why not <Link to="/create-profile" >Create a profile</Link>?</p>
+                        <button onClick={this.handleLogout} className="btn btn-danger">Logout</button>
+                    </div>
                 </div>
             </div>
+            // <div className="container test">
+            //     <div className="left-boarder">
+            //         <h1>{this.props.user.user}</h1>
+            //         <img className="profile-image" src={this.props.user.profile_picture} alt="username"/>
+            //         <p>If you haven't yet created a profile, why not <Link to="/create-profile">Create a
+            //             Profile</Link></p>
+            //         <button onClick={this.handleLogout} className="btn btn-danger">Logout</button>
+            //     </div>
+            //     <div className="right-boarder">
+            //         <p className="about-text" >{this.props.user.about_me}</p>
+            //     </div>
+            // </div>
         );
     }
 }
