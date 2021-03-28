214b73911df994bc02f5e8a21dea4eff7d5c877b
PatrickBoynton <jpboynton3@gmail.com>
2021-03-27 23:12:15 -0400

    Added the stars.

--- ephemeris/tasks.py
+++ ephemeris/tasks.py
@@ -5,7 +5,6 @@ from skyfield.almanac import find_discrete, risings_and_settings
 from pytz import timezone
 from datetime import datetime, timedelta
 
-
 # Planets
 sun = Ephem.objects.get(name='Sun')
 mercury = Ephem.objects.get(name='Mercury')
@@ -18,10 +17,25 @@ uranus = Ephem.objects.get(name='Uranus')
 neptune = Ephem.objects.get(name='Neptune')
 
 # Stars
+betelgeuse = Ephem.objects.get(name='Betelgeuse')
+sirius = Ephem.objects.get(name='Sirius')
+andromeda = Ephem.objects.get(name='Andromeda')
+orion_nebula = Ephem.objects.get(name="Orion's Nebula")
+pleadies = Ephem.objects.get(name='The Pleadies')
 
 
 eph = load('de421.bsp')
-planet = eph['mercury']
+planet = eph['sun']
+# Betelgeuse
+# star = Star(ra_hours=(5, 55, 10.3053), dec_degrees=(7, 24, 25.4))
+# Sirius
+# star = Star(ra_hours=(6, 45, 8.9), dec_degrees=(-16, 42, 58))
+# Andromeda Galaxy
+# star = Star(ra_hours=(0, 42, 44.3), dec_degrees=(41, 16.9))
+# Orion's Nebula
+# star = Star(ra_hours=(5, 35.4), dec_degrees=(-5, 27))
+# The Pleadies
+star = Star(ra_hours=(3, 47, 24), dec_degrees=(24, 7, 0))
 
 
 @app.shared_task
@@ -30,7 +44,8 @@ def get_ephem_times():
 
     # Updates the date.
     today = datetime.today().strftime('%Y-%m-%d').split('-')
-    tomorrow = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d').split('-')
+    tomorrow = (datetime.today() + timedelta(days=1)).strftime(
+        '%Y-%m-%d').split('-')
 
     t0 = ts.utc(int(today[0]), int(today[1]), int(today[2]))
     t1 = ts.utc(int(tomorrow[0]), int(tomorrow[1]), int(tomorrow[2]))
@@ -40,12 +55,13 @@ def get_ephem_times():
     greenville = wgs84.latlon(34.8526 * N, 82.3940 * W, elevation_m=299.923)
     global planet
 
-    f = risings_and_settings(eph, planet, greenville)
+    f = risings_and_settings(eph, star, greenville)
     tz = timezone('US/Eastern')
 
     for t, updown in zip(*find_discrete(t0, t1, f)):
         dates.append(t.astimezone(tz).strftime('%Y-%m-%d %H:%M'))
-    print(tomorrow)
-    mercury.rise_time = dates[0]
-    mercury.set_time = dates[1]
-    mercury.save()
+    print(dates)
+    pleadies.rise_time = dates[0]
+    pleadies.set_time = dates[1]
+
+    pleadies.save()
