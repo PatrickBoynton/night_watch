1c1ad7a0a62359d8c37b292854bf100629724361
PatrickBoynton <jpboynton3@gmail.com>
2021-03-28 13:22:35 -0400

    Attempting to make tests work.

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 # Create your tests here.
-class CustomUserTest(TestCase):
+class CustomUserTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
@@ -11,23 +11,21 @@ class CustomUserTest(TestCase):
             email='jpb3@example.com',
             password='safepass1'
         )
-
         self.assertEqual(user.username, 'jpb3')
         self.assertEqual(user.email, 'jpb3@example.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
-    def test_create_superuser(self):
+    def test_create_super_user(self):
         User = get_user_model()
-        admin_user = User.objects.create_superuser(
+        user = User.objects.create_superuser(
             username='admin',
             email='admin@example.com',
             password='safepass1'
         )
-
-        self.assertEqual(admin_user.username, 'admin')
-        self.assertEqual(admin_user.email, 'admin@example.com')
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
+        self.assertEqual(user.username, 'admin')
+        self.assertEqual(user.email, 'admin@example.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
