1b7ad5ea34a481da801fd4ee00c4a63db44783e1
PatrickBoynton <jpboynton3@gmail.com>
2021-03-29 14:24:05 -0400

    Added a subscription.

--- ephemeris/serializers.py
+++ ephemeris/serializers.py
@@ -4,6 +4,12 @@ from .models import Ephem
 
 
 class EphemSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField('is_subscriber')
+
+    def is_subscriber(self, obj):
+        request = self.context.get('request', None)
+        return request.user in obj.subscribers.all()
+
     class Meta:
         model = Ephem
         fields = '__all__'
