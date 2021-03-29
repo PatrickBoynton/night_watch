6a29ece785cc5b4244a207bc1895a87963ae5096
PatrickBoynton <jpboynton3@gmail.com>
2021-03-29 13:03:23 -0400

    Updated the fields to be nullable.

--- ephemeris/models.py
+++ ephemeris/models.py
@@ -4,12 +4,12 @@ from accounts.models import CustomUser
 
 # Create your models here.
 class Ephem(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, blank=True, null=True)
     rise_time = models.DateTimeField(null=True, blank=True)
     set_time = models.DateTimeField(null=True, blank=True)
     is_visible = models.BooleanField()
-    magnitude = models.FloatField()
-    image = models.ImageField(upload_to='ephemeris/')
+    magnitude = models.FloatField(blank=True, null=True)
+    image = models.ImageField(upload_to='ephemeris/', blank=True, null=True)
     description = models.TextField()
     subscribers = models.ManyToManyField(CustomUser)
 
