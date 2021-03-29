98e2c8ae876fa234b60cdbfd3009460199d807fd
PatrickBoynton <jpboynton3@gmail.com>
2021-03-29 16:14:20 -0400

    Made everything nullable

--- ephemeris/models.py
+++ ephemeris/models.py
@@ -7,21 +7,22 @@ User = get_user_model()
 
 # Create your models here.
 class Ephem(models.Model):
+
     name = models.CharField(max_length=40, blank=True, null=True)
     rise_time = models.DateTimeField(null=True, blank=True)
     set_time = models.DateTimeField(null=True, blank=True)
     is_visible = models.BooleanField()
     magnitude = models.FloatField(blank=True, null=True)
     image = models.ImageField(upload_to='ephemeris/', blank=True, null=True)
-    description = models.TextField()
-    subscribers = models.ManyToManyField(User)
+    description = models.TextField(blank=True, null=True)
+    subscribers = models.ManyToManyField(User, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Test(models.Model):
-    name = models.CharField(max_length=20)
+    # name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
