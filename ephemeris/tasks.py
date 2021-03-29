f80105c565f251c0f5487002aa106b0392aaa349
PatrickBoynton <jpboynton3@gmail.com>
2021-03-29 07:38:10 -0400

    Working on automating the update process.

--- ephemeris/tasks.py
+++ ephemeris/tasks.py
@@ -23,23 +23,48 @@ andromeda = Ephem.objects.get(name='Andromeda')
 orion_nebula = Ephem.objects.get(name="Orion's Nebula")
 pleadies = Ephem.objects.get(name='The Pleadies')
 
-
 eph = load('de421.bsp')
-planet = eph['sun']
+planet1 = eph['sun']
+planet2 = eph['mercury']
+planet3 = eph['venus']
+planet4 = eph['mars']
+# Jupiter
+planet5 = eph[5]
+# Saturn
+planet6 = eph[6]
+# Uranus
+planet7 = eph[7]
+# Neptune
+planet8 = eph[8]
+
+planets = [planet1,
+           planet2,
+           planet3,
+           planet4,
+           planet5,
+           planet6,
+           planet7,
+           planet8]
 # Betelgeuse
-# star = Star(ra_hours=(5, 55, 10.3053), dec_degrees=(7, 24, 25.4))
+bet = Star(ra_hours=(5, 55, 10.3053), dec_degrees=(7, 24, 25.4))
 # Sirius
-# star = Star(ra_hours=(6, 45, 8.9), dec_degrees=(-16, 42, 58))
+sir = Star(ra_hours=(6, 45, 8.9), dec_degrees=(-16, 42, 58))
 # Andromeda Galaxy
-# star = Star(ra_hours=(0, 42, 44.3), dec_degrees=(41, 16.9))
+androm = Star(ra_hours=(0, 42, 44.3), dec_degrees=(41, 16.9))
 # Orion's Nebula
-# star = Star(ra_hours=(5, 35.4), dec_degrees=(-5, 27))
+orion = Star(ra_hours=(5, 35.4), dec_degrees=(-5, 27))
 # The Pleadies
-star = Star(ra_hours=(3, 47, 24), dec_degrees=(24, 7, 0))
+plead = Star(ra_hours=(3, 47, 24), dec_degrees=(24, 7, 0))
+
+stars = [bet,
+         sir,
+         androm,
+         orion,
+         plead]
 
 
 @app.shared_task
-def get_ephem_times():
+def get_planet_times():
     ts = load.timescale()
 
     # Updates the date.
@@ -53,15 +78,42 @@ def get_ephem_times():
     dates = []
 
     greenville = wgs84.latlon(34.8526 * N, 82.3940 * W, elevation_m=299.923)
-    global planet
+    # global planet
 
-    f = risings_and_settings(eph, star, greenville)
+    f = risings_and_settings(eph, planets[0], greenville)
     tz = timezone('US/Eastern')
 
     for t, updown in zip(*find_discrete(t0, t1, f)):
         dates.append(t.astimezone(tz).strftime('%Y-%m-%d %H:%M'))
-    print(dates)
-    pleadies.rise_time = dates[0]
-    pleadies.set_time = dates[1]
 
-    pleadies.save()
+    sun.rise_time = dates[0]
+    sun.set_time = dates[1]
+    sun.save()
+
+
+@app.shared_task
+def get_star_times():
+    ts = load.timescale()
+
+    # Updates the date.
+    today = datetime.today().strftime('%Y-%m-%d').split('-')
+    tomorrow = (datetime.today() + timedelta(days=1)).strftime(
+        '%Y-%m-%d').split('-')
+
+    t0 = ts.utc(int(today[0]), int(today[1]), int(today[2]))
+    t1 = ts.utc(int(tomorrow[0]), int(tomorrow[1]), int(tomorrow[2]))
+
+    dates = []
+
+    greenville = wgs84.latlon(34.8526 * N, 82.3940 * W, elevation_m=299.923)
+    # global planet
+
+    f = risings_and_settings(eph, stars[0], greenville)
+    tz = timezone('US/Eastern')
+
+    for t, updown in zip(*find_discrete(t0, t1, f)):
+        dates.append(t.astimezone(tz).strftime('%Y-%m-%d %H:%M'))
+
+    betelgeuse.rise_time = dates[0]
+    betelgeuse.set_time = dates[1]
+    betelgeuse.save()
