328eb8f1e28d53a2761371cb31f1452cdce82bc4
PatrickBoynton <jpboynton3@gmail.com>
2021-03-28 20:51:08 -0400

    Updated the model.

--- events/models.py
+++ events/models.py
@@ -18,7 +18,8 @@ class Event(models.Model):
 
     name = models.CharField(max_length=60)
     ephemeris = models.CharField(max_length=30, null=True, blank=True)
-    time = models.TimeField(null=True, blank=True)
+    # time = models.TimeField(null=True, blank=True)
+    date_of_event = models.DateTimeField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE, null=True, blank=True)
