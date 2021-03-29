6f75be0a07f4995c3ce5b77eb927bff0c0c81239
PatrickBoynton <jpboynton3@gmail.com>
2021-03-29 10:29:49 -0400

    Worked on sms.

--- broadcast/views.py
+++ broadcast/views.py
@@ -2,15 +2,12 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from twilio.rest import Client
-from rest_framework import generics, permissions
-from .models import Appointment
 
 
 # Create your views here.
 @csrf_exempt
 def broadcast_sms(request):
     message_to_broadcast = request
-
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
     for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
