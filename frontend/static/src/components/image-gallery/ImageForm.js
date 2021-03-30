89afb54b08a57a862f71cc8512a8cb3ea4dad1f7
PatrickBoynton <jpboynton3@gmail.com>
2021-03-30 13:01:14 -0400

    Added an images section.

---
+++ frontend/static/src/components/image-gallery/ImageForm.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class ImageForm extends Component{
+    render() {
+        return (
+            <div>
+                ImageForm
+            </div>
+        );
+    }
+}
+
+export default ImageForm
