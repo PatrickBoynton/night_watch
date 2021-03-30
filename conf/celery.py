e6e268ceaddde242d185d6f369de81fb1c065099
PatrickBoynton <jpboynton3@gmail.com>
2021-03-30 12:02:22 -0400

    Called the right task.

--- conf/celery.py
+++ conf/celery.py
@@ -1,6 +1,5 @@
 import os
 from celery import Celery
-from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
@@ -8,21 +7,21 @@ app = Celery('conf', broker='redis://localhost:6379')
 
 app.config_from_object('django.conf:settings', namespace='Celery')
 
-app.autodiscover_tasks(['ephemeris.celery_app'])
+app.autodiscover_tasks(['ephemeris.celery_app', 'events.celery_app'])
 
 app.conf.beat_schedule = {
-    'print_rise_and_set_times': {
-        'task': 'ephemeris.tasks.get_planet_times',
-        # 'schedule': crontab(minute=0, hour=0),
-        'schedule': 120.00
-    },
-    'get_star_rise_and_set_times': {
-        'task': 'ephemeris.tasks.get_star_times',
-        'schedule': 121.00
-    },
+    # 'print_rise_and_set_times': {
+    #     'task': 'ephemeris.tasks.get_planet_times',
+    #     # 'schedule': crontab(minute=0, hour=0),
+    #     'schedule': 120.00
+    # },
+    # 'get_star_rise_and_set_times': {
+    #     'task': 'ephemeris.tasks.get_star_times',
+    #     'schedule': 121.00
+    # },
     'send_sms': {
-        'task': 'broadcasts.tasks.send_sms',
-        'schedule': ''
+        'task': 'events.tasks.message_user',
+        'schedule': 60.0
     }
 }
 
