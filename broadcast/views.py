de2d33a5fe813b5f45ce94058deb9e8865427501
PatrickBoynton <jpboynton3@gmail.com>
2021-03-30 20:15:38 -0400

    Added a check for the event type.

--- broadcast/views.py
+++ broadcast/views.py
@@ -7,16 +7,34 @@ from twilio.rest import Client
 # Create your views here.
 @csrf_exempt
 def broadcast_sms(data):
+
     # construct your message using the data object that is passed in
     # send message to phone number that is passed in on the data object
     # import pdb; pdb.set_trace()
-    recipient = data['phone']
-    message = data['event_name']
-    # message_to_broadcast = request
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    client.messages.create(to=recipient,
-                           from_=settings.TWILIO_NUMBER,
-                           body=message)
+
+    if 'name' in data:
+        print('name')
+        recipient = data['phone']
+        message = data['name']
+        # message_to_broadcast = request
+        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        client.messages.create(to=recipient,
+                               from_=settings.TWILIO_NUMBER,
+                               body=message)
+    if 'event_name' in data:
+        # print('event name')
+        recipient = data['phone']
+        message = data['event_name']
+        # message_to_broadcast = request
+        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        client.messages.create(to=recipient,
+                               from_=settings.TWILIO_NUMBER,
+                               body=message)
+
+
+    # import pdb
+    # pdb.set_trace()
+    #
     #
     # for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
     #     if recipient:
