602c2052930d55f68ca62416e0af55af6457c808
PatrickBoynton <jpboynton3@gmail.com>
2021-04-01 13:38:47 -0400

    Updated the profile.

--- accounts/models.py
+++ accounts/models.py
@@ -8,28 +8,18 @@ class CustomUser(AbstractUser):
     pass
 
 
+class CustomUserToken:
+    pass
+
+
 class Profile(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True,
-                                        null=True)
-    about_me = models.TextField(null=True, blank=True)
-
-    receive_notifications = models.BooleanField(null=True,
-                                                blank=True,
-                                                default=False)
-
-    location = models.CharField(max_length=50,
-                                null=True,
-                                blank=True,
-                                default='Greenville')
 
     phone = models.CharField(max_length=11,
                              null=True,
                              blank=True,
                              default='18646405680')
-
-    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
